9bcb9eca20a2ec44cd88991805980a0c92b77ed1
Wp_devsite <leshiy_pos@mail.ru>
2025-08-10 23:30:09 +0300

    finished main menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  //   Кнопка выпадающего меню Язык
-  dropDownToogle(
-    ".language-dropdown .dropdownToggle",
-    ".language-dropdown .dropdownMenu"
-  );
-  //   Кнопка выпадающего меню Валюты
-  dropDownToogle(
-    ".сurrency-dropdown .dropdownToggle",
-    ".сurrency-dropdown .dropdownMenu"
-  );
-});
-
 function dropDownToogle(butSelect, menuSelect) {
   let butLang = document.querySelector(butSelect);
   let dropLang = document.querySelector(menuSelect);
@@ -31,3 +18,43 @@ function dropDownToogle(butSelect, menuSelect) {
     }
   });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  //   Кнопка выпадающего меню Язык
+  dropDownToogle(
+    ".language-dropdown .dropdownToggle",
+    ".language-dropdown .dropdownMenu"
+  );
+  //   Кнопка выпадающего меню Валюты
+  dropDownToogle(
+    ".сurrency-dropdown .dropdownToggle",
+    ".сurrency-dropdown .dropdownMenu"
+  );
+
+  //   Главное меню
+  let listMenu = document.querySelectorAll(".main_menu > ul > li");
+
+  listMenu.forEach((item) => {
+    let subMenu = item.querySelector(".dropdawnWrap");
+    let closeMenu;
+
+    if (!subMenu) return;
+
+    item.addEventListener("mouseenter", () => {
+      let openSubMenus = document.querySelectorAll(".main_menu .visible");
+
+      if (openSubMenus.length) {
+        clearTimeout(closeMenu);
+        openSubMenus.forEach((sm) => sm.classList.remove("visible"));
+      }
+
+      subMenu.classList.add("visible");
+    });
+
+    item.addEventListener("mouseleave", () => {
+      closeMenu = setTimeout(() => {
+        subMenu.classList.remove("visible");
+      }, 1000);
+    });
+  });
+});
