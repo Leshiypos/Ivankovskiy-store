37298d78c3b6050d2aa2a3c74c554df89e421f51
Wp_devsite <leshiy_pos@mail.ru>
2025-08-12 13:20:20 +0300

    Desktop version of the main page is finished

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -1,7 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const swiper = new Swiper(".stripe_slider", {
-    speed: 400,
-    slidesPerView: 5,
-    spaceBetween: 10,
-  });
+  let sliders_stripe = document.querySelectorAll(".stripe_slider");
+
+  if (sliders_stripe) {
+    sliders_stripe.forEach((slider) => {
+      new Swiper(slider, {
+        speed: 400,
+        slidesPerView: 5,
+        spaceBetween: 10,
+      });
+    });
+  }
+
+  let sliders_section = document.querySelectorAll(
+    ".slider_section .img_slider"
+  );
+  console.log(sliders_section);
+  if (sliders_section) {
+    sliders_section.forEach((slider) => {
+      new Swiper(slider, {
+        speed: 400,
+        loop: true,
+        slidesPerView: "auto",
+        spaceBetween: 10,
+      });
+    });
+  }
 });
