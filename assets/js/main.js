3106f6b6a1c49619fbdfb6ecf759d177541204f2
Wp_devsite <leshiy_pos@mail.ru>
2025-09-03 09:28:47 +0300

    Fix 3D model on the product page

--- assets/js/main.js
+++ assets/js/main.js
@@ -487,8 +487,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // 3D визуализация
   // Слайдеры картинок
-  document.querySelectorAll("model-viewer").forEach((mv) => {
-    mv.addEventListener("mouseenter", () => mv.dismissPoster());
-    mv.addEventListener("mouseleave", () => mv.showPoster());
-  });
+  const models = document.querySelectorAll("model-viewer");
+  if (models) {
+    models.forEach((mv) => {
+      if (!mv.classList.contains("product_page")) {
+        mv.addEventListener("mouseenter", () => mv.dismissPoster());
+        mv.addEventListener("mouseleave", () => mv.showPoster());
+      }
+    });
+  }
 });
